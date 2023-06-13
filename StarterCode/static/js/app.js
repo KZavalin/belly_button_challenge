87e804513adab5f3599d965c435e36897fc6cfcb
KZavalin <kirill.zavalin@gmail.com>
2023-06-13 01:17:54 -0500

    bar working

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,9 +2,9 @@ const navellink = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-clas
 d3.json(navellink).then(function(data) {
     console.log(data);
   });
-const json=d3.json(navellink);
-console.log(json);  
-const dataset=json.samples;
+const jsona=d3.json(navellink);
+console.log(jsona);  
+const dataset=jsona.samples;
 console.log(dataset);
 dropdownMenu=d3.select("#selDataset");
 
@@ -19,24 +19,26 @@ function init() {
           for (var i=0; i < options.length; i++) {
            dropdownMenu.append('option').text(options[i]).property('value',i)
         }
-    
-    let selection = 940;
-    let personID = 1;
-    let persondata = dataset[1];
-    let top10ids= persondata.otu_ids.slice(0, 10);
+       
+    let selection = d3.select("#selDataset");
+    let personID = selection.property("value");
+    let persondata = dataset[personID];
+    let selectionID = options[personID];
+    console.log(selectionID)
     let top10numbers= persondata.sample_values.slice(0, 10);
+    let top10ids = persondata.otu_ids.slice(0, 10).map(id=>`OTU ${id}`);
     let top10labels= persondata.otu_labels.slice(0, 10);
     let trace1 = {
         y: top10ids,
         x: top10numbers,
-        text: top10ids,
-        name: `Top 10 OTUs for Individual ${selection}`,
+        text: top10labels,
+        name: `Top 10 OTUs for Individual ${selectionID}`,
         type: "bar",
         orientation: "h",
                 };
     let traceData = [trace1]
     let layout = {
-        title: `Top 10 OTUs for Individual ${selection}`,
+        title: `Top 10 OTUs for Individual ${selectionID}`,
         margin: {
           l: 100,
           r: 100,
@@ -56,34 +58,43 @@ d3.selectAll("#selDataset").on("change", updatePlotly);
 
 // This function is called when a dropdown menu item is selected
 function updatePlotly() {
+    console.log('change detected')
     d3.json(navellink).then(function(data) {
-        let options=data.names;
         let dataset = data.samples;
-          for (var i=0; i < options.length; i++) {
-           dropdownMenu.append('option').text(options[i]).property('value',i)
-        }
-    
-    
+        let options=data.names;
   // Use D3 to select the dropdown menu
   selection = d3.select("#selDataset");
   // Assign the value of the dropdown menu option to a variable
   personID = selection.property("value");
-  persondata = dataset[personID]
+  console.log(personID);
+  persondata = dataset[personID];
+  let selectionID = options[personID];
+  console.log(selectionID)
 
     // Use D3 to select the dropdown menu
 
-    let top10ids= persondata.otu_ids.slice(0, 10);
     let top10numbers= persondata.sample_values.slice(0, 10);
+    let top10ids = persondata.otu_ids.slice(0, 10).map(id=>`OTU ${id}`);
     let top10labels= persondata.otu_labels.slice(0, 10);
-    let update = {
-        x: top10ids,
-        y: top10numbers,
+    let trace1 = {
+        y: top10ids,
+        x: top10numbers,
         text: top10labels,
-        name: "Top 10 OTUs for Individual ${selection}",
-      };
-      Plotly.restyle('bar',update)
-
-    // Assign the value of the dropdown menu option to a variable
+        name: `Top 10 OTUs for Individual ${selectionID}`,
+        type: "bar",
+        orientation: "h",
+                };
+    let traceData = [trace1]
+    let layout = {
+        title: `Top 10 OTUs for Individual ${selectionID}`,
+        margin: {
+          l: 100,
+          r: 100,
+          t: 100,
+          b: 100
+            }
+        };
+    Plotly.react("bar", traceData, layout);
   
 });};
   
